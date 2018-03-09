2b5de5980274f82fc987ec065aaed5b3ab2d89c0
Robin_Li <13994367734@163.com>
2018-03-09 14:02:50 +0800

    commit

--- js/learn.js
+++ js/learn.js
@@ -1,3 +1,28 @@
 window.onload = function(){
-  
+  // var arr = new Array();
+  // console.log(arr);
+  // arr[0] = "aaa";
+  // arr[1] = "bbb";
+  // arr[2] = "ccc";
+  // arr[3] = "ddd";
+  // console.log(arr.length);
+
+  var obj = new Object();
+  obj.name = "wang";
+  obj.age = 12;
+  obj.gender = "女";
+  console.log(obj.name);
+  console.log(obj);
+
+  // 二位数组
+  var arr = new Array();
+  for(var i = 0; i < 9; i++){
+    arr[i] = new Array();
+    for(var j = 0; j < 9; j++){
+      arr[i][j] = i+j
+    }
+  }
+  console.log(arr);
+
+
 }
